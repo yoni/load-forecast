1514c1e3bd2d9e91547985d8c59bba5289071dd9
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2014-03-18 20:23:44 -0400

    Move API key into a separate configuration file.

--- src/python/wunderground.py
+++ src/python/wunderground.py
@@ -5,7 +5,11 @@ A weather forecast retrieval utility.
 import urllib2
 import json
 
-f = urllib2.urlopen('http://api.wunderground.com/api/82a3a7f51c0128b0/geolookup/conditions/q/IA/Cedar_Rapids.json')
+config_file =open('./config/wunderground.json')
+config = json.load(config_file)
+config_file.close()
+
+f = urllib2.urlopen('http://api.wunderground.com/api/%s/geolookup/conditions/q/IA/Cedar_Rapids.json' % config['key'])
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
