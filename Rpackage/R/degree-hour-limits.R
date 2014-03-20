da815d1958deec0d30a186add98c823cf68cbe6b
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2014-03-20 15:31:09 -0400

    Cleanup on aisle R. Take 3.

--- Rpackage/R/degree-hour-limits.R
+++ Rpackage/R/degree-hour-limits.R
@@ -1,6 +1,8 @@
 #' Calculates temperature limits for plots.
 #' @param balance_point the balance point to use in calculating the high and low limits of expected degree hours
 #' @export
+#' @examples
+#' limits <- degree_hour_limits()
 degree_hour_limits <- function(balance_point=BALANCE_POINT) {
     c(-(balance_point - LOW_TEMPERATURE), HIGH_TEMPERATURE - balance_point)
 }
